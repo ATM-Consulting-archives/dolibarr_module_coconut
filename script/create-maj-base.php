3772373952c6fc18475b6482fe6776469651b519
gauthier <gauthier.verdol@atm-consulting.fr>
2017-04-26 14:51:06 +0200

    init mod

---
+++ script/create-maj-base.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Script créant et vérifiant que les champs requis s'ajoutent bien
+ */
+
+if(!defined('INC_FROM_DOLIBARR')) {
+	define('INC_FROM_CRON_SCRIPT', true);
+
+	require('../config.php');
+
+}
+
+
+/* uncomment
+
+
+dol_include_once('/coconut/class/coconut.class.php');
+
+$PDOdb=new TPDOdb;
+
+$o=new Tcoconut;
+$o->init_db_by_vars($PDOdb);
+
+$o=new TcoconutChild;
+$o->init_db_by_vars($PDOdb);
+*/
